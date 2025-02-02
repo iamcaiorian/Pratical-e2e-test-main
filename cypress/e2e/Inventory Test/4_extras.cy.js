48b2946e01cbd48478cee2c0d1684afcbf241e73
iamcaiorian <caio.rianbr@gmail.com>
2025-02-02 17:48:45 -0300

    test: validated each product's name, price, and image [extras]

--- cypress/e2e/Inventory Test/4_extras.cy.js
+++ cypress/e2e/Inventory Test/4_extras.cy.js
@@ -10,4 +10,12 @@ describe('Inventory - Extras', () => {
   it('Should validate the presence of all products on the inventory page', () => {
     cy.get('.inventory_item').should('have.length', 6);
   });
+
+  it('Should validate that each product has a name, price, and image', () => {
+    cy.get('.inventory_item').each(($item) => {
+      cy.wrap($item).find('.inventory_item_name').should('be.visible');
+      cy.wrap($item).find('.inventory_item_price').should('be.visible');
+      cy.wrap($item).find('img').should('have.attr', 'src');
+    });
+  });
 });
